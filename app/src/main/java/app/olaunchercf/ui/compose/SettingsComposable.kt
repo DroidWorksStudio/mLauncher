29eb4ffa76750621433d1e54c1a56c6a28a77371
Joscha Loos <j.loos@posteo.net>
2022-09-21 22:38:58 +0200

    chore: update dependencies

--- app/src/main/java/app/olaunchercf/ui/compose/SettingsComposable.kt
+++ app/src/main/java/app/olaunchercf/ui/compose/SettingsComposable.kt
@@ -21,8 +21,6 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
