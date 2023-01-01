d195003042110e6ccb1eca38673f2904a59cc99f
The-Repo-Club <wayne6324@gmail.com>
2023-01-01 04:34:41 +0000

    Added feathericons support (#147)

--- app/src/main/java/com/github/hecodes2much/mlauncher/ui/HomeFragment.kt
+++ app/src/main/java/com/github/hecodes2much/mlauncher/ui/HomeFragment.kt
@@ -14,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.os.bundleOf
 import androidx.core.view.children
 import androidx.core.view.size
@@ -159,6 +160,7 @@ class HomeFragment : Fragment(), View.OnClickListener, View.OnLongClickListener 
         } else binding.firstRunTips.visibility = View.GONE
 
         with(viewModel) {
+
             clockAlignment.observe(viewLifecycleOwner) { gravity ->
                 binding.dateTimeLayout.gravity = gravity.value()
             }
