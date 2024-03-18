655890472b7414b2760c830ab090ad4d31192d7d
HeCodes2Much <wayne6324@gmail.com>
2024-03-18 11:43:35 +0000

    [Release] new release
    Signed-off-by: HeCodes2Much  <wayne6324@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.mlauncher"
         minSdk = 23
         targetSdk = 34
-        versionCode = 71
-        versionName = "1.3.4"
+        versionCode = 72
+        versionName = "1.3.5"
     }
 
     buildTypes {
