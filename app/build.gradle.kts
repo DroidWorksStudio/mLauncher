d42b457b5c08083c660c76cce3692e6e37a4958c
HeCodes2Much <wayne6324@gmail.com>
2024-04-11 17:43:24 +0100

    chore(release): prepare for version 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.mlauncher"
         minSdk = 23
         targetSdk = 34
-        versionCode = 150
-        versionName = "1.5.0"
+        versionCode = 151
+        versionName = "1.5.1"
     }
 
     buildTypes {
