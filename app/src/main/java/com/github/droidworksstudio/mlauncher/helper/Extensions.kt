2006a167982f4ab0d37d117acf09c252539e8f38
HeCodes2Much <wayne6324@gmail.com>
2024-04-27 10:42:11 +0100

    refactor: Remove unused imports and empty lines in Extensions.kt
    - Removed unused imports and empty lines for improved code cleanliness and readability.

--- app/src/main/java/com/github/droidworksstudio/mlauncher/helper/Extensions.kt
+++ app/src/main/java/com/github/droidworksstudio/mlauncher/helper/Extensions.kt
@@ -4,13 +4,9 @@ import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.LauncherApps
-import android.content.res.Configuration
-import android.hardware.Sensor
-import android.hardware.SensorManager
 import android.net.Uri
 import android.os.UserHandle
 import android.view.View
-import android.view.ViewConfiguration
 import android.view.inputmethod.InputMethodManager
 import com.github.droidworksstudio.mlauncher.data.Constants
 
@@ -62,7 +58,6 @@ fun Context.searchOnPlayStore(query: String? = null): Boolean {
         false
     }
 }
-
 
 fun Context.isPackageInstalled(packageName: String, userHandle: UserHandle = android.os.Process.myUserHandle()): Boolean {
     val launcher = getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
