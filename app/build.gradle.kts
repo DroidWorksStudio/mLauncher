39170a8175095944b5a66834c748001328f4e3a3
HeCodes2Much <wayne6324@gmail.com>
2024-04-30 18:49:45 +0100

    chore(release): prepare for version 1.5.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.mlauncher"
         minSdk = 23
         targetSdk = 34
-        versionCode = 158
-        versionName = "1.5.8"
+        versionCode = 159
+        versionName = "1.5.9"
     }
 
     buildTypes {
