59fdf1d2f9dbef8fa5f7ba4ef3c3a7410252cec7
CreativeCodeCat <wayne6324@gmail.com>
2025-05-28 14:57:17 +0100

    Feat: Add FontMap for custom fonts
    This commit introduces a `FontMap` object to manage custom fonts used within the application.
    
    - The `FontMap` provides a mapping between font names (String) and their corresponding resource IDs (Int).
    - Currently, it includes the following fonts:
        - arbutus
        - ubuntu
        - ubuntu_light
        - ubuntu_condensed_regular
        - workbench

---
+++ app/src/main/java/com/github/droidworksstudio/launcher/utils/FontMap.kt
@@ -0,0 +1,13 @@
+package com.github.droidworksstudio.launcher.utils
+
+import com.github.droidworksstudio.launcher.R
+
+object FontMap {
+    val fonts: Map<String, Int> = mapOf(
+        "arbutus" to R.font.arbutus,
+        "ubuntu" to R.font.ubuntu,
+        "ubuntu_light" to R.font.ubuntu_light,
+        "ubuntu_condensed_regular" to R.font.ubuntu_condensed_regular,
+        "workbench" to R.font.workbench,
+    )
+}
