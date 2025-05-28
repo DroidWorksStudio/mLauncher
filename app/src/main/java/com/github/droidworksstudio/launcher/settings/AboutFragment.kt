cee0bbd12a6a5372c6cf47857caa1014b877508c
CreativeCodeCat <wayne6324@gmail.com>
2025-05-28 14:53:35 +0100

    Refactor: Simplify app info navigation and add title
    This commit refactors the AboutFragment:
    
    - Replaced the manual intent creation for opening app details with the more modern `LauncherApps.startAppDetailsActivity`. This simplifies the code and aligns with current Android development practices.
    - Implemented the `TitleProvider` interface to provide a title for the fragment, enhancing consistency within the settings UI.

--- app/src/main/java/com/github/droidworksstudio/launcher/settings/AboutFragment.kt
+++ app/src/main/java/com/github/droidworksstudio/launcher/settings/AboutFragment.kt
@@ -1,18 +1,18 @@
 package com.github.droidworksstudio.launcher.settings
 
-import android.content.Intent
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.LauncherApps
 import android.os.Bundle
-import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import com.github.droidworksstudio.launcher.R
 import com.github.droidworksstudio.launcher.utils.StringUtils
 
-class AboutFragment : Fragment() {
+class AboutFragment : Fragment(), TitleProvider {
 
     private val stringUtils = StringUtils()
 
@@ -26,6 +26,8 @@ class AboutFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val launcherApps = requireActivity().getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps
+
         // Set up about page links
         stringUtils.setLink(requireActivity().findViewById(R.id.creditText), getString(R.string.my_website_link))
         stringUtils.setLink(requireActivity().findViewById(R.id.gitlabLink), getString(R.string.gitlab_link))
@@ -35,10 +37,16 @@ class AboutFragment : Fragment() {
         stringUtils.setLink(requireActivity().findViewById(R.id.weatherLink), getString(R.string.weather_link))
 
         requireActivity().findViewById<ImageView>(R.id.iconView).setOnClickListener {
-            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                data = "package:${requireContext().packageName}".toUri()
-            }
-            startActivity(intent)
+            launcherApps.startAppDetailsActivity(
+                ComponentName(requireContext(), this::class.java),
+                launcherApps.profiles[0],
+                null,
+                null
+            )
         }
+    }
+
+    override fun getTitle(): String {
+        return getString(R.string.about_title)
     }
 }
