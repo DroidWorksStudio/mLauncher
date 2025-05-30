d4f123e68813a45a46c05343612ce56d393f1961
CreativeCodeCat <wayne6324@gmail.com>
2025-05-30 15:13:13 +0100

    Refactor: Change version type to production
    The application version type in `app/build.gradle.kts` has been updated from beta (`1`) to production (`0`).
    
    Signed-off-by: CreativeCodeCat <wayne6324@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ val minor = 10
 val patch = 5
 val build = 1
 
-val type = 1 // 1=beta, 2=alpha else=production
+val type = 0 // 1=beta, 2=alpha else=production
 
 val baseVersionName = "$major.$minor.$patch.$build"
 
