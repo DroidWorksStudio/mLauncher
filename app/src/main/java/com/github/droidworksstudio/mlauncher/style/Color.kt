c393adde8f837efc8a85d2ed2340151060830956
CreativeCodeCat <wayne6324@gmail.com>
2025-06-06 14:52:41 +0100

    Refactor: Adjust header text colors for light and dark themes
    The `textLightHeader` color has been changed from `0xFF27529D` to `0xFF1A3C91` for improved visibility in light mode.
    
    The `textDarkHeader` color has been updated from `0xFF27529D` to `0xFF7CAEFF` to enhance contrast and readability in dark mode.
    
    Signed-off-by: CreativeCodeCat <wayne6324@gmail.com>

--- app/src/main/java/com/github/droidworksstudio/mlauncher/style/Color.kt
+++ app/src/main/java/com/github/droidworksstudio/mlauncher/style/Color.kt
@@ -2,8 +2,8 @@ package com.github.droidworksstudio.mlauncher.style
 
 import androidx.compose.ui.graphics.Color
 
-val textLightHeader = Color(0xFF27529D)
+val textLightHeader = Color(0xFF1A3C91)
+val textDarkHeader = Color(0xFF7CAEFF)
 val textLight = Color(0xFFFFFFFF)
-val textDarkHeader = Color(0xFF27529D)
 val textDark = Color(0xFF000000)
 val textGray = Color(0xFF858585)
